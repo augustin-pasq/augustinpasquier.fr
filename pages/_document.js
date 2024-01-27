8b9a8802f415950aafe1a9616acab0d3cf0d3d01
Augustin Pasquier <pasquieraugustin@outlook.fr>
2024-01-27 22:20:04 +0100

    Remove unused module

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,12 @@
 import {Head, Html, Main, NextScript} from "next/document"
-import {BreakpointProvider} from "react-socks";
 
 export default function Document() {
     return (
         <Html lang="fr">
             <Head/>
             <body>
-            <BreakpointProvider>
                 <Main/>
                 <NextScript/>
-            </BreakpointProvider>
             </body>
         </Html>
     )
