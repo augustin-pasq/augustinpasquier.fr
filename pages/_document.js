b121a79bd3b8b6e26863000975bf92f12874310d
Augustin Pasquier <pasquieraugustin@outlook.fr>
2024-02-05 23:48:08 +0100

    Add OpenGraph

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,23 @@ import {Head, Html, Main, NextScript} from "next/document"
 export default function Document() {
     return (
         <Html lang="fr">
-            <Head/>
+            <Head>
+                <meta name="description" content="👋, moi c'est Augustin Pasquier, et je suis étudiant en BUT informatique, développeur web en alternance et passionné par la programmation."/>
+                <link rel="icon" href="/favicon.ico"/>
+
+                <meta property="og:url" content="https://augustinpasquier.fr"/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="Augustin Pasquier | Portfolio"/>
+                <meta property="og:description" content="👋, moi c'est Augustin Pasquier, et je suis étudiant en BUT informatique, développeur web en alternance et passionné par la programmation."/>
+                <meta property="og:image" content="https://augustinpasquier.fr/images/opengraph-image.png"/>
+
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:domain" content="augustinpasquier.fr"/>
+                <meta property="twitter:url" content="https://augustinpasquier.fr"/>
+                <meta property="twitter:title" content="Augustin Pasquier | Portfolio"/>
+                <meta property="twitter:description" content="👋, moi c'est Augustin Pasquier, et je suis étudiant en BUT informatique, développeur web en alternance et passionné par la programmation."/>
+                <meta property="twitter:image" content="https://augustinpasquier.fr/images/opengraph-image.png"/>
+            </Head>
             <body>
                 <Main/>
                 <NextScript/>
